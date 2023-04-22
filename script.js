bcf6aed6555da6157e32ebf87b4f4702c311dcfc
PurpleCactuar <121975596+PurpleCactuar@users.noreply.github.com>
2023-04-22 11:42:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-			// Define the original array
-			var origArray = [2, 3, 4];
+		// original array
+		var origArray = [2, 3, 4];
 
-			// Write the values of the origArray into the 3 textboxes
+		// put in the textboxes
 			document.getElementById("textbox1").value = origArray[0];
 			document.getElementById("textbox2").value = origArray[1];
 			document.getElementById("textbox3").value = origArray[2];
 
-			// Add event listeners for the 2nd and 3rd buttons
+		// event listeners for 2nd and 3rd buttons
 			document.getElementById("button2").addEventListener("click", function() {
-				// Cube the values in the origArray
-				var newArray = origArray.map(function(x) {
-					return x * x * x;
+			// Cube the values
+			var newArray = origArray.map(function(x) {
+				return x * x * x;
 				});
 
-				// Write out the new values into the HTML textboxes
-				document.getElementById("textbox1").value = newArray[0];
-				document.getElementById("textbox2").value = newArray[1];
-				document.getElementById("textbox3").value = newArray[2];
+			// new values
+			document.getElementById("textbox1").value = newArray[0];
+			document.getElementById("textbox2").value = newArray[1];
+			document.getElementById("textbox3").value = newArray[2];
 			});
 
 			document.getElementById("button3").addEventListener("click", fourthPowerValues);
 		});
 
 		function squareValues() {
-			// Define the original array
+			// original array
 			var origArray = [2, 3, 4];
 
-			// Square the values in the origArray
+			// square the values
 			var newArray = origArray.map(function(x) {
 				return x * x;
 			});
 
-			// Write out the new values into the HTML textboxes
+			// new values
 			document.getElementById("textbox1").value = newArray[0];
 			document.getElementById("textbox2").value = newArray[1];
 			document.getElementById("textbox3").value = newArray[2];
 		}
 
 		function fourthPowerValues() {
-			// Define the original array
+			// original array
 			var origArray = [2, 3, 4];
 
-			// Take the 4th power of the values in the origArray
+			// 4th power
 			var newArray = origArray.map(function(x) {
 				return Math.pow(x, 4);
 			});
 
-			// Write out the new values into the HTML textboxes
+			// new values
 			document.getElementById("textbox1").value = newArray[0];
 			document.getElementById("textbox2").value = newArray[1];
 			document.getElementById("textbox3").value = newArray[2];
